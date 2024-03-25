917d0d884022327087ec152d8cd467e957380a4d
AGozhan <AGozhan@mtsbank.ru>
2024-03-25 21:07:51 +0300

    setup logger (slog)
    setup logger (slog)

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"urlshortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envTest  = "test"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	_ = cfg
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
 
 	// todo init logger (slog)
 
@@ -16,4 +27,33 @@ func main() {
 	// todo init router (chi)
 
 	// todo run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envTest:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+	return log
 }
