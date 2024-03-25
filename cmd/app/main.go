58e21dcbcd2245b5bb07665d0cdda07f3bc3ec0c
AGozhan <AGozhan@mtsbank.ru>
2024-03-25 18:54:46 +0300

    add gitignore

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
+	// todo init config (cleanenv)
 
+	// todo init logger (slog)
+
+	// todo init storage (sqlite)
+
+	// todo init router (chi)
+
+	// todo run server
 }
